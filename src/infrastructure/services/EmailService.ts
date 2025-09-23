860f9b8545ef0d9e0c2abab7037395218d055b6e
Natheer_sharawi <111202840+Natheer777@users.noreply.github.com>
2025-09-23 18:12:42 +0300

    Add files via upload

--- src/infrastructure/services/EmailService.ts
+++ src/infrastructure/services/EmailService.ts
@@ -11,9 +11,6 @@ export class EmailService {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      },
-      tls: {
-        rejectUnauthorized: false
       }
     });
   }
