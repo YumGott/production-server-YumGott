e02e7ae8f555d6a330910738b470602f1bf87041
Ibrahem326 <90620210+Ibrahem326@users.noreply.github.com>
2025-09-24 05:34:58 +0300

    Update EmailService.ts

--- src/infrastructure/services/EmailService.ts
+++ src/infrastructure/services/EmailService.ts
@@ -11,7 +11,9 @@ export class EmailService {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      }
+      },
+      debug: true,
+      logger: true
     });
   }
 
